4dd04a3f79b0c872c7c3c62d16db9a0df31baa9e
Matt Shildt <matt.shildt@gmail.com>
2017-04-10 22:10:25 -0400

    Fixed some bugs
    Still needs lots of love.

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 import logging
-from random import randint
+from random import shuffle
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 app = Flask(__name__)
-ask = Ask(app, '/')
+ask = Ask(app, '/dinnerbot')
 
 test_data = [
   'spaghetti and meatballs',
@@ -28,17 +28,26 @@ def index():
 def dinnerbot_launch():
     welcome_msg = render_template('welcome')
     dinner_list = test_data[:]
-    dinner_list.shuffle()
+    shuffle(dinner_list)
     session.attributes['dinners'] = dinner_list
     return question(welcome_msg)
 
-@ask.intent("StartIntent")
+@ask.intent("SuggestionsIntent")
 def start_suggesting():
+    try:
+        dinners = session.attributes['dinners']
+    except KeyError:
+        dinner_list = test_data[:]
+        shuffle(dinner_list)
+        session.attributes['dinners'] = dinner_list
+        dinners = session.attributes['dinners']
+
     try:
         dinner = session.attributes['dinners'].pop()
     except IndexError:
         return statement("I'm out of ideas.")
 
+    session.attributes['last'] = dinner
     suggestion_msg = render_template('suggestion', dinner=dinner)
 
     return question(suggestion_msg)
@@ -50,14 +59,21 @@ def next_suggestion():
     except IndexError:
         return statement("I'm out of ideas.")
 
+    session.attributes['last'] = dinner
     suggestion_msg = render_template('suggestion', dinner=dinner)
 
     return question(suggestion_msg)
 
 @ask.intent("YesIntent")
 def finish():
+    if 'last' in session.attributes:
+        dinner = session.attributes['last']
+    else:
+        dinner = 'whatever you are having'
+
     enjoy_msg = render_template('enjoy', dinner=dinner)
     return statement(enjoy_msg)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
